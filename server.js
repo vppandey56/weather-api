6de4c40b14c4e4afced9907a2af2d5e3897b9b39
vppandey56 <vppandey5660@gmail.com>
2026-04-21 00:27:21 +0530

    Fixed CORS issue

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors()); // ✅ FIX: allow frontend requests
+
 const PORT = process.env.PORT || 3000;
 
 const states = [
